a67f1aa15177a17f80866a29db5cd6a645d9829e
Rajjaa <elhamdani.rajaa@gmail.com>
2022-06-17 13:06:32 +0200

    modified hpool stage: poolblocks

--- pytorch_geometric/graphgym/custom_graphgym/stage/hierapool_stage.py
+++ pytorch_geometric/graphgym/custom_graphgym/stage/hierapool_stage.py
@@ -1,83 +1,57 @@
+from collections import defaultdict
 from math import ceil
 
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 import torch_geometric.graphgym.register as register
 from torch_geometric.graphgym.config import cfg
-from torch_geometric.graphgym.init import init_weights
-from torch_geometric.graphgym.models.layer import (
-    BatchNorm1dNode,
-    GeneralLayer,
-    GeneralMultiLayer,
-    new_layer_config,
-)
 from torch_geometric.graphgym.register import register_stage
-from torch_geometric.utils import to_dense_batch, to_dense_adj
 
 
 @register_stage('hierarchical_pooling')
 class HieraPoolStage(nn.Module):
     """
-    Stage that stack GNN layers and pooling layers
-    and a flat pooling layer after the last GNN layer
+    Stage that stacks GNN Layers interweaved with pooling layers and after pooling layers
 
     Args:
         dim_in (int): Input dimension
         dim_out (int): Output dimension
         num_layers (int): Number of GNN layers
     """
-    def __init__(self, dim_in, dim_out, num_layers):
+    def __init__(self, dim_in, dim_out, num_blocks):
         super().__init__()
-        GNNLayer = register.layer_dict['gnn_layer']
-        HieraPoolLayer = register.pooling_dict[cfg.hierarchical_pooling.type]
-
-        self.num_layers = num_layers
+        PoolBlock = register.pooling_dict[f'{cfg.hierarchical_pooling.type}_block']
+        self.num_blocks = num_blocks
+        self.post_pool = cfg.hierarchical_pooling.num_post_pool_layers != 0
+        PostPoolBlock = register.layer_dict['gnn_dense_block']
 
         max_num_nodes = cfg.dataset.max_num_nodes
         pool_ratio = cfg.hierarchical_pooling.pool_ratio
-        no_new_clusters = ceil(pool_ratio * max_num_nodes)
+        no_new_nodes = ceil(pool_ratio * max_num_nodes)
+                
 
-        for i in range(num_layers):
+        for i in range(self.num_blocks):
             d_in = dim_in if i == 0 else dim_out
-            # use dense layers for GNN layers after the first pooling layer
             dense = i != 0
-            gnn_layer = GNNLayer(d_in, dim_out, dense=dense)
-            self.add_module('gnn_layer{}'.format(i), gnn_layer)
-            if i != self.num_layers-1:
-                pool_layer = HieraPoolLayer(dim_out, no_new_clusters)
-                self.add_module('pool_layer{}'.format(i), pool_layer)
-                no_new_clusters = ceil(pool_ratio * no_new_clusters)
-        
+            pool_block = PoolBlock(d_in, dim_out, no_new_nodes, dense=dense)
+            self.add_module(f'pool_block{i}', pool_block)
+            no_new_nodes = ceil(pool_ratio * no_new_nodes)
+            if self.post_pool:
+                post_pool_block = PostPoolBlock(dim_out, dim_out,
+                 cfg.hierarchical_pooling.num_post_pool_layers, dense=True)
+                self.add_module(f'post_pool_block{i}', post_pool_block)
+
 
     def forward(self, batch):
         """"""
-        for i in range(self.num_layers):
-            if cfg.hierarchical_pooling.type == 'mincutpool':
-                batch.aux_loss = {
-                    'o': 0,
-                    'mc': 0
-                }
-            gnn_layer = self.get_submodule(f'gnn_layer{i}'.format(i))
-            if i == 0:
-                # forward through the message passing layers
-                batch = gnn_layer(batch)
-                x, edge_index = batch.x, batch.edge_index
-                x, mask = to_dense_batch(x, batch.batch)
-                adj = to_dense_adj(edge_index, batch.batch)
-                batch.x = x
-                batch.mask = mask
-                batch.adj = adj
-                # forward through the pooling layer
-                pool_layer = self.get_submodule(f'pool_layer{i}'.format(i))
-                batch = pool_layer(batch)
-            elif i != self.num_layers-1:
-                batch = gnn_layer(batch)
-                pool_layer = self.get_submodule(f'pool_layer{i}'.format(i))
-                batch = pool_layer(batch)
-            else:
-                batch = gnn_layer(batch)
-        if cfg.gnn.l2norm:
-            batch.x = F.normalize(batch.x, p=2, dim=-1)
+        if cfg.model.aux_loss:
+            batch.aux_loss = defaultdict(int)
+
+        for i in range(self.num_blocks):
+            pool_block = self.get_submodule(f'pool_block{i}')
+            to_dense = i == 0
+            batch = pool_block(batch, to_dense)
+            if self.post_pool:
+                post_pool_block = self.get_submodule(f'post_pool_block{i}')
+                batch = post_pool_block(batch)
         return batch
