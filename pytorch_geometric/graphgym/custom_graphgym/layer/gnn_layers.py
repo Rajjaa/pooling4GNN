a617836537cc6008e2a58cadd96834ec3b1710eb
Rajjaa <elhamdani.rajaa@gmail.com>
2022-06-17 12:04:24 +0200

    added GNNBlock

--- pytorch_geometric/graphgym/custom_graphgym/layer/gnn_layers.py
+++ pytorch_geometric/graphgym/custom_graphgym/layer/gnn_layers.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch_geometric as pyg
+import torch_geometric.graphgym.register as register
 from torch_geometric.graphgym.config import cfg
 from torch_geometric.graphgym.models.layer import (
     GeneralLayer,
@@ -25,6 +26,26 @@ def GNNLayer(dim_in, dim_out, has_act=True, dense=False):
         layer_type,
         layer_config=new_layer_config(dim_in, dim_out, 1, has_act=has_act,
                                       has_bias=False, cfg=cfg))
+
+
+@register_layer('gnn_dense_block')
+class GNNBlock(nn.Module):
+    def __init__(self, dim_in, dim_out, num_layers, final_act=True, dense=False):
+        super().__init__()
+        self.num_layers = num_layers
+
+        for i in range(num_layers):
+            d_in = dim_in if i == 0 else dim_out
+            has_act = i!=num_layers-1 or final_act
+            gnn_layer = GNNLayer(d_in, dim_out, dense=dense, has_act=has_act)
+            self.add_module(f'gnn_layer{i}', gnn_layer)
+
+    
+    def forward(self, batch, mask=None):
+        """"""
+        for i, module in enumerate(self.children()):
+            batch = module(batch)
+        return batch
 
 
 @register_layer('graphconv')
