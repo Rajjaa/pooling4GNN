edf7f077e139b349bd189063d4a151842afcb487
Rajjaa <elhamdani.rajaa@gmail.com>
2022-08-10 10:26:18 +0200

    custom dataset and model config

--- pytorch_geometric/graphgym/custom_graphgym/config/custom_config.py
+++ pytorch_geometric/graphgym/custom_graphgym/config/custom_config.py
@@ -6,8 +6,14 @@ from torch_geometric.graphgym.register import register_config
 @register_config('custom')
 def set_cfg_pooling(cfg):
     """
-    This function sets the default values for customized pooling layers
+    This function sets the default values for customized config values
     """
+    # ----------------------------------------------------------------------- #
+    # dataset options
+    # ----------------------------------------------------------------------- #
+
+    cfg.dataset.subset = False
+
     # ----------------------------------------------------------------------- #
     # train options
     # ----------------------------------------------------------------------- #
@@ -15,4 +21,9 @@ def set_cfg_pooling(cfg):
     # whether there is an auxiliary loss computed by the model forward
     cfg.model.aux_loss = False
 
-    cfg.model.gradient_clip_val=0.0
+    cfg.model.gradient_clip_val = 0.0
+
+
+    # ----------------------------------------------------------------------- #
+    # gnn options
+    # ----------------------------------------------------------------------- #
